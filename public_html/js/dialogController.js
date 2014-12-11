8acace41e163ad37f6590615816a647679bbb76e
ginkgoMZD <mzd@ginkgostreet.com>
2014-12-11 14:28:44 -0500

    remove unused property and custom action

--- public_html/js/dialogController.js
+++ public_html/js/dialogController.js
@@ -4,18 +4,12 @@ var DialogControllerBase = {
     console.log('DialogController.init');
   },
   title: 'Find Local Help',
-  step: 1,
   user_prompt: 'Need help with your health insurance application? Enter your ZIP code below to find appointments with local application assisters.',
   actions: {
-    search: function() {
-      console.log('DialogController.search');
-      console.dir(this.get('userInput'));
-      this.transitionToRoute('dialog.locator');
-    }
   },
   userInput: {
     near: null,
-    radius: 10,
+    radius: 100,
   }
 }
 
