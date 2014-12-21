e52d18fbd5d3013ed4c1bcc85c8c2ca1d9df51df
ginkgoMZD <mzd@ginkgostreet.com>
2014-12-20 19:52:58 -0500

    display partner locations first; NOTE: paging seems broken;

--- public_html/js/locationIndexController.js
+++ public_html/js/locationIndexController.js
@@ -5,20 +5,34 @@ var LocationIndexRouteBase = {
   },
   model: function() {
     if (!this.modelPrereqCheck()) return false;
+    var otherResults = {};
+    var thisRoute = this;
     var ctrlLocationIndex = this.controllerFor('location.index');
-    var data = this.controllerFor('location').get('userInput');
-    data.partner_id = 32;
-    data.page = ctrlLocationIndex.get('currentPage');
-    return Ember.$.ajax({
+    var params = this.ajaxRequestParams();
+    params.data = this.controllerFor('location').get('userInput');
+    params.data.page = ctrlLocationIndex.get('currentPage');
+    return Ember.$.ajax(params)
+    .done(function(result){
+      thisRoute.controllerFor('locationIndex').set('otherResults', result);
+      ctrlLocationIndex.set('hasOtherResults', (result.results.length > 0));
+
+      //Now get Partner Results
+      params.data.partner = 32;
+      return Ember.$.ajax(params)
+      .done(function(result) {
+        thisRoute.controllerFor('locationIndex').set('partnerResults', result);
+        ctrlLocationIndex.set('hasPartnerResults', (result.results.length > 0));
+        return result;
+      });
+    });
+  },
+  ajaxRequestParams: function() {
+    return {
       type: 'GET',
       url: 'https://connector.getcoveredamerica.org/api/locations/',
       crossDomain: true,
       dataType: 'json',
-      data: data,
-    }).success(function(result) {
-      ctrlLocationIndex.set('hasResults', true);
-      return result;
-    });
+    }
   },
   actions: {
     searchLocations: function() {
@@ -46,7 +60,13 @@ var LocationIndexControllerBase = {
   * @type type
   */
  location: {},
- hasResults: false,
+ partnerResults: {},
+ otherResults: {},
+ hasOtherResults: false,
+ hasPartnerResults: false,
+ showResults: function() {
+   return (this.hasOtherResults || this.hasPartnerResults);
+ }.property('hasOtherResults', 'hasPartnerResults'),
  currentPage: 1,
  actions: {
     scheduleAppointment: function(location) {
