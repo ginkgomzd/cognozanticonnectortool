127c153438a3b14507fb8b71886d02440c284607
ginkgoMZD <mzd@ginkgostreet.com>
2015-01-06 11:43:40 -0500

    remove dev default-values

--- public_html/js/locationModel.js
+++ public_html/js/locationModel.js
@@ -4,9 +4,7 @@ var LocationBase = {
     console.log('LocationModel.init');
   },
   userInput: {
-//    near: 99501, //TESTING ALASKA
-//    near: 85012, //testing Phoenix
-    near: 33614, // Testing Tampa
+    near: '',
     radius: 10,
     language: ''
   }
