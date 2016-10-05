46e663c631f8ab59b0682dfd612d42114972e45c
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-10-04 21:49:45 -0700

    Converted application template to use i18n

--- connector/app/locales/en/translations.js
+++ connector/app/locales/en/translations.js
@@ -8,5 +8,15 @@ export default {
   // },
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
+  
+
+  "app": {
+    "title": "Schedule an Appointment",
+    "legal": "Enrollment services are provided without discrimination on the basis of race, color, national origin, sex, age, sexual orientation, gender identity, or disability.",
+    "about1": "The Health Insurance Marketplace is the way to find health care coverage that fits your budget and meets your needs.",
+    "about2": "Whether you're uninsured, or just want to explore your healthcare choices, the Marketplace will help find coverage that's right for you.",
+    "cognosante-tag": "Cognosante - Minds on Health",
+  },
+
 
 };
