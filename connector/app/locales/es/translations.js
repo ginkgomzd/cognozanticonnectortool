26256ab5f03cde6970325e317240e57531bc0b5a
BrianDombrowski <bdombro@gmail.com>
2016-10-10 21:33:09 -0400

    CC2-15 #comment added translations and tweaked template

--- connector/app/locales/es/translations.js
+++ connector/app/locales/es/translations.js
@@ -8,4 +8,116 @@ export default {
   // },
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+  "location": {
+    "title": "Encuentra Ayuda en Persona",
+    "copy": "¿Necesitas ayuda con tu solicitud de seguro de salud? Escribe tu código postal abajo para hacer una cita con alguien que te pueda ayudar en persona con tu solicitud.",
+  },
+
+  "available": {
+    "title": "Ubicación seleccionada",
+    "copy": "",
+  },
+
+  "confirm": {
+    "thank-you": "¡Gracias por registrarse para una cita!",
+    "on-your-way": "¡Está en camino de obtener cobertura! Aquí está la información de su cita.",
+    "what-to-bring-link": "Haga clic aquí para obtener una lista de información para traer a su próxima cita.",
+    "change-cancel": "Si necesita cambiar o cancelar su cita, por favor llame",
+    "add-to-calendar": "Añadir al calendario",
+  },
+
+  "form-controls": {
+    "zipcode": "Código postal",
+    "search-radius": "Distancia",
+    "language": {
+      "label": "Idioma",
+      "options": {
+        "tel": "Traducción de teléfono (todos los idiomas)",
+        "all": "Todo",
+      }
+    },
+    "application-type": {
+      "label": "Tipo de aplicación",
+      "options": {
+        "individual": "Individual o familiar",
+        "group": "Grupo (aplicaciones separadas)",
+        "shop": "Pequeñas empresas (SHOP)",
+      }
+    },
+    "start-date": "Desde fecha",
+    "end-date": "A fecha",
+    "first-name": {
+      "label": "Nombre",
+      "placeholder": "Nombre",
+    },
+    "last-name": {
+      "label": "Apellido",
+      "placeholder": "Apellido",
+    },
+    "email": {
+      "label": "Dirección de correo electrónico",
+      "placeholder": "user@domain.com",
+    },
+    "phone": {
+      "label": "Número de teléfono",
+      "placeholder": "202-456-1111",
+    },
+    "opt-in-email": "Recibir alertas por correo electrónico de Get Covered America",
+    "buttons": {
+      "search-for-help": "Buscar ayuda",
+      "search-appointments": "Buscar citas",
+      "schedule-online": "Programar en línea",
+      "schedule": "Programar",
+      "find-location": "Encuentre una ubicación",
+      "back": "Atrás",
+      "select": "Seleccionar",
+      "default": "Buscar",
+    },
+    "results": {
+      "next-available": "Próxima cita disponible",
+      "no-available": "No hay citas disponibles",
+    },
+  },
+
+  "errors": {
+    "no-location": "No fue posible encontrar la ubicación seleccionada. Por favor regrese a la página de búsqueda y vuelva intentarlo.",
+    "no-appointment": "Lo siento, tuvimos algunos problemas cargando esa hora de cita. Por favor, inténtelo de nuevo.",
+    "no-confirmation": "Lo siento, algo ha ido mal. Por favor, inténtelo de nuevo.",
+    "continued": "Si continúa recibiendo este error, por favor, pónganse en contacto con el administrador del sistema.",
+    "valid-phone": "Por favor, especifique un número de teléfono válido.",
+    "no-results": "No se encontraron resultados.",
+    "default": "Un error desconocido ocurrió.",
+  },
+
+  "app": {
+    "title": "Programar una cita",
+    "legal": "Servicios de inscripción se proporcionan sin discriminación por motivos de raza, color, origen nacional, sexo, edad, orientación sexual, identidad de género o discapacidad.",
+    "about1": "El Mercado de Seguros Médicos es la vía por la cual puede encontrar cobertura médica que se ajuste a su presupuesto y satisfaga sus necesidades.",
+    "about2": "Si usted no tiene seguro, o si sólo quiere explorar sus opciones de seguro médico, el Mercado le ayudará encontrar la cobertura que sea adecuada para usted.",
+    "cognosante-tag": "Cognosante - Mente en la salud",
+  },
+
+  "widgets": {
+    "maplink": {
+      "text": "Ver mapa",
+      "hover": "Buscar la dirección en Google Maps",
+    },
+    "prev-next": {
+      "prev": "Página anterior",
+      "next": "Siguiente página",
+    }
+  },
+
+  "words": {
+    "accommodations": "Acomodaciones",
+    "time": "Hora",
+    "location": "Ubicación",
+    "select": "Seleccionar",
+  },
+
+  "misc": {
+
+  }
+
 };
